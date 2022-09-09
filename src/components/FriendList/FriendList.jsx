76d784dedc271ca0c84133711d5d18b0ccdd974f
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-09-09 21:35:55 +0300

    Summaty

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
+
+function FriendList({ friends }) {
+   if (true) {
+      console.log(friends);
+
+   }
+
+   return (
+      <ul className={styles['friend-list']}>
+         {friends.map(({ avatar, name, isOnline, id }) => (
+            <FriendListItem
+               avatar={avatar}
+               name={name}
+               isOnline={isOnline}
+               key={id}
+            />
+         ))}
+      </ul>
+   );
+}
+
+FriendList.propTypes = {
+   friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default FriendList;
